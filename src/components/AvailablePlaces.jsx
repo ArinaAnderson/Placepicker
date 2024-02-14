cc2241d8d9d4ba92afcf835b603b72abb58e776d
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-02-14 15:17:19 -0800

    replace promise with async and await in fetchAllPlaces

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,17 +3,25 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then(response => response.json())
-      .then(data => {
-        setPlaces(data.places)
-      })
+    const fetchAllPlaces = async () => {
+      setIsLoading(true);
+      const availablePlacesData = await fetch('http://localhost:3000/places');
+      const availablePlaces = await availablePlacesData.json();
+      setPlaces(availablePlaces.places);
+      setIsLoading(false);
+    };
+    fetchAllPlaces();
+    
   }, []);
   return (
     <Places
       title="Available Places"
       places={places}
+      loadingText="Fetching place data..."
+      isLoading={isLoading}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
